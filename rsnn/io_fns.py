b4e09c09e05a5e20bbf1621fa268706c60b8e525
w <wkvanderveen>
2020-09-29 16:57:41 +0200

    Start working on RSNN

---
+++ rsnn/io_fns.py
@@ -0,0 +1,6 @@
+import numpy as np
+from config import config
+def random_input():
+	rng = np.random.default_rng()
+
+	return rng.integers(2, size=config["N_I"])
---
+++ rsnn/rsnn.py
@@ -0,0 +1,32 @@
+"""
+Construct the network
+
+"""
+
+from config import config
+import io_fns
+import numpy as np
+
+rng = np.random.default_rng()
+
+x = np.zeros(size=(config['N_I']))
+s = np.zeros(size=(config['N_rec'], config['N_R'], 2))
+ev = np.zeros(size=(config['N_rec'], config['N_R'], 2))
+W_rec = rng.random(size=(config['N_rec'], config['N_R'], config['N_R']))
+W_ffd = rng.random(size=(config['N_rec']-1, config['N_R'], config['N_R']))
+W_in = rng.random(size=(config['N_I'], config['N_R']))
+W_out = rng.random(size=(config['N_R'], config['N_O']))
+B = rng.random(size=(config['N_rec'], config['N_R']))
+
+vtilde = lambda v, z: v - (v + 65) * z
+utilde = lambda u, z: u + 2 * z
+
+t = 0
+
+while t < config["N_E"]:
+    x = io_fns.random_input()
+        
+
+
+
+    t += 1
