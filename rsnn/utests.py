696cf35b19e1544163a7dd18d3ba6511640b1655
wkvanderveen <w.k.van.der.veen.2@student.rug.nl>
2020-10-05 14:43:01 +0200

    Traub Izh now seems to work with dt

--- rsnn/utests.py
+++ rsnn/utests.py
@@ -11,6 +11,162 @@ rng = np.random.default_rng()
 def U(a, b):
     return a + rng.random() * (b - a)
 
+def traub_izh_np():
+    fig, axs = plt.subplots(8, 1)
+
+    log_v1 = []
+    log_v2 = []
+    log_evec_v1 = []
+    log_evec_u1 = []
+    log_evec_v2 = []
+    log_evec_u2 = []
+    log_h2 = []
+    log_etrace1 = []
+    log_grad1 = []
+    log_etrace2 = []
+    log_grad2 = []
+    log_u1 = []
+    log_u2 = []
+    log_z1 = []
+    log_z2 = []
+
+    num = 2
+    Nv = np.ones(shape=(num,)) * -config["IzhV1"]
+    Nu = np.zeros(shape=(num,))
+    Nz = np.zeros(shape=(num,))
+    H = np.zeros(shape=(num,))
+    TZ = np.zeros(shape=(num,))
+    I = np.zeros(shape=(num,))
+
+    rng = np.random.default_rng()
+
+    W = rng.random(size=(num, num,))
+    EVv = np.zeros(shape=W.shape)
+    EVu = np.zeros(shape=W.shape)
+    ET = np.zeros(shape=W.shape)
+    G = np.zeros(shape=W.shape)
+
+    T = 500
+    A = 5/2
+    B = 10/2
+    C = 15/2
+    D = 10
+
+    for t in range(0, T):
+        # if t < T * 0.33:
+        #     I[0] = A
+        #     I[1] = B
+        #     I[2] = C
+        #     if TZ[0] >= TZ[1]:
+        #         I[1] = D
+        # elif t < T * 0.67:
+        #     I[0] = B
+        #     I[1] = C
+        #     I[2] = A
+        #     if TZ[1] >= TZ[2]:
+        #         I[2] = D
+
+        # else:
+        #     I[0] = C
+        #     I[1] = A
+        #     I[2] = B
+        #     if TZ[1] >= TZ[2]:
+        #         I[2] = D
+        if t < T * 0.5:
+            I[0] = C
+            I[1] = A
+            if TZ[0] >= TZ[1]:
+                I[1] = D
+        else:
+            I[0] = A
+            I[1] = C
+            if TZ[1] >= TZ[0]:
+                I[0] = D
+
+        Nvp = (Nv - (Nv + 65) * Nz) + config["dt"] * (0.04 * (Nv - (Nv + 65) * Nz)**2
+                                                      + 5 * (Nv - (Nv + 65) * Nz)
+                                                      + 140
+                                                      - (Nu + 2 * Nz)
+                                                      + I)
+
+        Nz = np.where(Nvp >= config["H1"], 1, 0)
+        TZ = np.where(Nvp >= config["H1"], TZ, t)
+
+        # Should this operate on pseudo?
+        Nun = (Nu + 2 * Nz) + config["dt"] * (0.004 * Nvp - 0.02 * (Nu + 2 * Nz))
+        Nvn = (Nvp - (Nvp + 65) * Nz) + config["dt"] * (0.04 * (Nvp - (Nvp + 65) * Nz)**2
+                                                        + 5 * (Nvp - (Nvp + 65) * Nz)
+                                                        + 140
+                                                        - (Nu + 2 * Nz)
+                                                        + I)
+
+        EVvn = (EVv * (1 - Nz
+                       + 0.08 * config["dt"] * Nvp
+                       - 0.08 * config["dt"] * Nvp * Nz  # not sure about Nvp here
+                       + 5 * config["dt"]
+                       - 5 * config["dt"] * Nz)
+                - EVu * config["dt"]
+                + Nz[np.newaxis].T * config["dt"])
+
+        EVun = (EVv * (0.004 * config["dt"]
+                       - 0.004 * config["dt"] * Nz)
+                + EVu * (1
+                         - 0.02 * config["dt"]))
+
+        H = config["gamma"] * np.exp((np.clip(Nvp, a_min=None, a_max=config["H1"]) - config["H1"])
+                                     / config["H1"])
+
+        EVv = EVvn
+        EVu = EVun
+
+        Nv = Nvn
+        Nu = Nun
+
+        ET = H * EVv
+
+        G = G + ET
+
+        log_v1.append(Nv[0])
+        log_v2.append(Nv[1])
+        log_evec_v1.append(EVv[0, 1])
+        log_evec_v2.append(EVv[1, 0])
+        log_evec_u1.append(EVu[0, 1])
+        log_evec_u2.append(EVu[1, 0])
+        log_etrace1.append(ET[0, 1])
+        log_etrace2.append(ET[1, 0])
+        log_grad1.append(G[0, 1])
+        log_grad2.append(G[1, 0])
+        log_h2.append(H[1])
+        log_u1.append(Nu[0])
+        log_u2.append(Nu[1])
+        log_z1.append(Nz[0])
+        log_z2.append(Nz[1])
+
+    axs[0].plot(log_v1)
+    axs[0].plot(log_v2)
+    axs[0].set_title("Voltage")
+    axs[1].plot(log_u1)
+    axs[1].plot(log_u2)
+    axs[1].set_title("Refractory")
+    axs[2].plot(log_z1)
+    axs[2].plot(log_z2)
+    axs[2].set_title("Spikes")
+    axs[3].plot(log_evec_v1)
+    axs[3].plot(log_evec_v2)
+    axs[3].set_title("Voltage eligibility")
+    axs[4].plot(log_evec_u1)
+    axs[4].plot(log_evec_u2)
+    axs[4].set_title("Refractory eligibility")
+    axs[5].plot(log_h2)
+    axs[5].set_title("Pseudo-derivative")
+    axs[6].plot(log_etrace1)
+    axs[6].plot(log_etrace2)
+    axs[6].set_title("Eligibility trace")
+    axs[7].plot(log_grad1)
+    axs[7].plot(log_grad2)
+    axs[7].set_title("Gradient")
+
+    plt.show()
 
 def traub_izh():
     fig, axs = plt.subplots(8, 1)
@@ -39,7 +195,7 @@ def traub_izh():
     et = 0
     grad = 0
 
-    T = 500
+    T = 5000
     tzi = 0
     tzo = 0
     A = 15/2
@@ -47,7 +203,7 @@ def traub_izh():
     C = 10
 
     for t in range(0, T):
-        if t < T * 0.3:
+        if t < T * 0.5:
             I_i = A
             I_o = B
             if tzi >= tzo:
@@ -78,7 +234,7 @@ def traub_izh():
         v2n = ut.vnext(v=v2_pseudo, u=u2, z=z2, I=I_o)
         u2n = ut.unext(v=v2_pseudo, u=u2, z=z2)
 
-        evvn = ut.evvnext(zi=z1, zj=z2, vi=v1, vj=v2, evv=evv, evu=evu)
+        evvn = ut.evvnext(zi=z1, zj=z2, vi=v1_pseudo, vj=v2_pseudo, evv=evv, evu=evu)
         evun = ut.evunext(zi=z1, zj=z2, evv=evv, evu=evu)
 
         evv = evvn
@@ -88,8 +244,8 @@ def traub_izh():
         u1 = u1n
         v2 = v2n
         u2 = u2n
-        h2 = ut.h(v=v2_pseudo)
 
+        h2 = ut.h(v=v2)
         et = h2 * evv
 
         grad = grad + et
